64f95b82ecab2d79594b9b20f413fbb624a501ef
devGusta2 <gustavo.souza2979@gmail.com>
2024-05-27 22:50:39 -0300

    Frequência simples

---
+++ Estatistica aplicada/frequencia-simples.py
@@ -0,0 +1,18 @@
+#imports
+from tkinter import *;
+from tkinter import ttk; #tree view vem daqui
+
+app=Tk()
+app.title("Frequencia simples")
+app.geometry("600x300")
+
+
+
+
+
+
+
+
+app.mainloop()
+
+
